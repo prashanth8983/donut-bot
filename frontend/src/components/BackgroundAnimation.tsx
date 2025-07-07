f5e5f1aeaeda2d6eabbb02ff445c027fe919f1ee
Prashanth Kumar <prashanth8983@gmail.com>
2025-07-06 20:32:44 -0400

    feat: add background animation component for visual appeal

---
+++ frontend/src/components/BackgroundAnimation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BackgroundAnimation: React.FC = () => {
+  return (
+    <div className="background-animation">
+      <div className="shape shape-1"></div>
+      <div className="shape shape-2"></div>
+      <div className="shape shape-3"></div>
+      <div className="shape shape-4"></div>
+      <div className="shape shape-5"></div>
+    </div>
+  );
+};
+
+export default BackgroundAnimation;
